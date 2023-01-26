e8ed5b23bf981a8fbc4cfc2940628023da0db7b4
hyemin <88538911+hhyem@users.noreply.github.com>
2023-01-26 23:03:03 +0900

    Update ex_enneagramWord.c

--- ex_enneagramWord.c
+++ ex_enneagramWord.c
@@ -1,3 +1,5 @@
+//영어 단어를 이루는 글자들이 뒤죽박죽 섞인 것을 받아서 순서대로 재배치 하는 게임
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +10,8 @@ int main() {
   char answer[100] = SOL;
   char user[100];
 
+  //문자열 안의 글자 섞기 > 난수 필요
+  //두 개의 난수를 발생시켜 그 위치의 글자들이 서로 바뀌도록 한다. 
   for(int i=0; i<strlen(answer); i++) {
     int r1 = rand()%strlen(answer);
     int r2 = rand()%strlen(answer);
