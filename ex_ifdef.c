5653dc0e32217611e40e2f352946afea7b016cd3
hyemin <88538911+hhyem@users.noreply.github.com>
2023-01-27 10:00:19 +0900

    Update ex_ifdef.c

--- ex_ifdef.c
+++ ex_ifdef.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #define PI 3.1415
-#define CIRECLE_AREA(r) (PI*r*r)
+#define CIRCLE_AREA(r) (PI*r*r)
 #define DEBUG
 
 int main(void) {
-    double radius, area;
+  double radius, area;
 
-    #ifdef DEBUG
-    	printf("디버깅 모드입니다.\n");
-
-	printf("원의 반지름 : ");
+  #ifdef DEBUG
+    printf("디버깅 모드입니다.\n");
+  #endif
+    printf("원의 반지름 : ");
     scanf("%lf", &radius);
     area = CIRCLE_AREA(radius);
     printf("원이 면적 = %.2f", area);
     
-    return 0;
+  return 0;
 }
