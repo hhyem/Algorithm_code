4ebb52e160e993985379aaa40cfa758efa773283
hyemin <88538911+hhyem@users.noreply.github.com>
2023-02-02 14:51:22 +0900

    Create ex_module_name.py

---
+++ ex_module_name.py
@@ -0,0 +1,23 @@
+#module01.py
+def fun():
+	print('module1 함수 실행 ')
+    
+if __name__ == '__main':  #메인 파일이면 실행한다.
+	fun()
+    print('__name__: ', __name__)
+    
+#module02.py
+def fun():
+	print('module2 함수 실행 ')
+    
+if __name__ == '__main':  #메인 파일이면 실행한다.
+	fun()
+    print('__name__: ', __name__)
+    
+#module03.py
+def fun():
+	print('module3 함수 실행 ')
+    
+if __name__ == '__main':  #메인 파일이면 실행한다.
+	fun()
+    print('__name__: ', __name__)
