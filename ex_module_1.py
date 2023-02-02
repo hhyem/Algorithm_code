8f87fa0385384155c1bbe8e67fba5a4c43fc8073
hyemin <88538911+hhyem@users.noreply.github.com>
2023-02-02 14:45:09 +0900

    Create ex_module_1.py

---
+++ ex_module_1.py
@@ -0,0 +1,13 @@
+def convertUnit(lenMm):
+	unitDic = {}
+    
+    unitDic['cm'] = lenMm * 0.1
+    unitDic['m'] = lenMm * 0.001
+    unitDic['inch'] = lenMm * 0.03937
+    unitDic['ft'] = lenMm & 0.003281
+    
+    return unitDic
+    
+def printLength(originalData, lenghts):
+	for len in lengths.keys():
+    	print(originalData, 'mm -->', lenghts[len], len)
