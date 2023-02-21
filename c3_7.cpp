475b20d5efa36bd3bf8bacf0d2a3d7a747f90b33
hyemin <88538911+hhyem@users.noreply.github.com>
2023-02-21 21:56:56 +0900

    Create c3_7.cpp

---
+++ c3_7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+  int x, y;
+public:
+  Point();
+  Point(int a, int b);
+  void show() {cout << "(" << x << "," << y << ")" << endl;}
+};
+
+Point::Point() : Point(0,0) { }   //위임생성자
+Point:: Point(int a, int b)       //타겟생성자
+  : x(a), y(b) { }
+
+int main() {
+  Point origin;
+  Point target(10, 20);
+  origin.show();
+  target.show();
+}
