dbe56e6b55726ab01620fb8a9149890965d2045e
hyemin <88538911+hhyem@users.noreply.github.com>
2023-03-09 15:15:33 +0900

    Create Q22.c

---
+++ Q22.c
@@ -0,0 +1,34 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxof(const int a[], int n) {
+  int i;
+  int max = a[0];
+  for(i=1; i<n; i++) 
+    if(a[i] > max) max = a[i];
+  return max;
+}
+
+int main(void) {
+  int i;
+  int *height;
+  int number;
+  
+  printf("사람 수 : ");
+  srand(time(NULL));
+  number = 5 + rand() % 16; //5 이상 20 이하 
+  printf("%d\n", number);
+  
+  height = calloc(number, sizeof(int));
+
+  for(i=0; i<number; i++) {
+    height[i] = 100 + rand() % 90; //100~189의 난수 생성, 대입
+    printf("height[%d] = %d\n", i, height[i]);
+  }
+
+  printf("최대값은 %d입니다. \n", maxof(height, number));
+  free(height);
+
+  return 0;
+}
