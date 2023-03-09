7d41a272e3d2cc0e31df30cb10408ddd99262056
hyemin <88538911+hhyem@users.noreply.github.com>
2023-03-09 15:11:42 +0900

    Create Q21.c

---
+++ Q21.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double aveof (const int a[], int n) {
+  int i;
+  int sum = 0;
+  
+  for(i=0; i<n; i++) 
+    sum += a[i];
+  return sum/n;
+}
+
+int main() {
+
+  int i;
+  int *height;
+  int number;
+  printf("사람 수 : ");
+  scanf("%d", &number);
+  height = calloc(number, sizeof(int));
+  printf("%d 사람의 키를 입력하세요 : \n", number);
+  for(i=0; i<number; i++) {
+    printf("height[%d] : ", i);
+    scanf("%d", &height[i]);
+  }
+
+  printf("키의 평균은 %f입니다.\n", aveof(height, number));
+  free(height);
+
+  return 0;
+  
+}
