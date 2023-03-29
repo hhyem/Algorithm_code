2e89a7f395e63c50ebb3a0570a00cab21af5fb75
hyemin <88538911+hhyem@users.noreply.github.com>
2023-03-29 12:23:35 +0900

    Create Q43.c

---
+++ Q43.c
@@ -0,0 +1,10 @@
+//int형 큐를 사용하는 프로그램에 임의의 데이털ㄹ 검색하는 함수 추가하기 
+int Search2(const IntQueue *q, int x) {
+	int i;
+
+	for (i = 0; i < q->num; i++) {
+		if (q->que[(i + q->front) % q->max] == x)
+			return i;		/* 검색 성공 */
+	}
+	return -1;				/* 검색 실패 */
+}
