dcd87a73794aab67c31a552466152d5d4038e72d
hyemin <88538911+hhyem@users.noreply.github.com>
2023-07-05 02:17:58 +0900

    Update Q18.c

--- Q18.c
+++ Q18.c
@@ -17,3 +17,31 @@ int main() {
     scanf("%d", &n);
     nspira(n);
 }
+
+//직접 짠 코드
+#include <stdio.h>
+
+void nrpira(int n) {
+  int t = (n-1) * 2 + 1; //한 줄에 출력할 숫자 개수 
+  int nt = 0, index, j = 1;
+  
+  for(int i=n; i>=1; i--) {
+    index = (i-1) * 2 + 1; //공백 개수 
+    nt = (t-index) / 2;       //별 개수 
+    for(int q=0; q<nt; q++)
+      printf(" ");
+    for(int z=0; z<index; z++)
+      printf("%d", j);
+    j+=1;
+    printf("\n");
+  }
+}
+
+int main(void) {
+
+  int n;
+  scanf("%d", &n);
+
+  nrpira(n);
+  
+}
