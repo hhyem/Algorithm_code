6f91f62a1e7e13361e0764e43bc163eee0446832
hyemin <88538911+hhyem@users.noreply.github.com>
2023-07-05 02:05:38 +0900

    Update Q17.c

--- Q17.c
+++ Q17.c
@@ -17,3 +17,31 @@ int main() {
     scanf("%d", &n);
     spira(n);
 }
+
+
+//아래는 직접 짠 코드
+#include <stdio.h>
+
+void spira(int n) {
+  int t = (n-1) * 2 + 1;
+  int nt = 0, index;
+  
+  for(int i=1; i<=n; i++) {
+    nt = (i-1) * 2 + 1; //별 개수 
+    index = (t-nt) / 2; //공백 개수 
+    for(int z=0; z<index; z++)
+      printf(" ");
+    for(int q=0; q<nt; q++)
+      printf("*");
+    printf("\n");
+  }
+}
+
+int main(void) {
+
+  int n;
+  scanf("%d", &n);
+
+  spira(n);
+  
+}
