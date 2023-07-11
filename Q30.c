f3636ce443963f47d170f06726979d623e44b6e0
hyemin <88538911+hhyem@users.noreply.github.com>
2023-07-11 13:07:49 +0900

    Update Q30.c

--- Q30.c
+++ Q30.c
@@ -93,3 +93,97 @@ int main(void) {
     Before(d[i], n);
   
 }
+
+//혼자 짠 코드
+#include <stdio.h>
+
+typedef struct {
+  int y;
+  int m;
+  int d;
+} Date;
+
+Date Dateof(int y, int m, int d) {
+  printf("%d년 %d월 %d일\n", y, m, d);
+}
+
+Date After(Date x, int n) {
+  int day = x.d;
+  day += n;
+  if(day >= 29) {
+    if(x.m == 2) {
+      day -= 29;
+    }
+    else if(day >= 31) {
+      if(x.m == 4 || x.m == 6 || x.m == 9 || x.m == 11) {
+        day -= 30;
+      }
+      else 
+        day -= 31;
+    }
+    x.m += 1;
+  }
+
+  if(x.m > 12) {
+    x.y += 1;
+    x.m = 1;
+  }
+
+  printf("%d일 뒤 날짜는 : %d년 %d월 %d일\n", n, x.y, x.m, day);
+}
+
+Date Before(Date x, int n) {
+  int day = x.d;
+  day -= n;
+
+  if(day <= 0) {
+    x.m -= 1;
+    if(x.m == 2) {
+      day += 28;
+    }
+    else if(x.m == 4 || x.m == 6 || x.m == 9 || x.m == 11) {
+      day += 30;
+    }
+    else {
+      day += 31;
+    }
+  }
+
+  if(x.m < 0) {
+    x.y -= 1;
+    x.m = 12;
+  }
+
+  printf("%d일 전 날짜는 : %d년 %d월 %d일\n", n, x.y, x.m, day);
+}
+
+int main() {
+
+  int after, before, day;
+  
+  Date d[] = {
+  {2017,5,25},
+  {2023,7,11},
+  {2002,5,31},
+  {1986,2,5},
+  {1999, 12, 31},
+  {2023,7,2}
+  };
+
+  int size = sizeof(d)/sizeof(d[0]);
+  
+  for(int i=0; i<size; i++)
+    Dateof(d[i].y, d[i].m, d[i].d);
+
+  printf("몇일 뒤 날짜 알기 : ");
+  scanf("%d", &after);
+  printf("몇번째 날 ? : ");
+  scanf("%d", &day);
+  After(d[day-1], after);
+
+  printf("몇일 전 날짜 알기 : ");
+  scanf("%d", &before);
+  printf("몇번째 날 ? : ");
+  scanf("%d", &day);
+  Before(d[day-1], before);
+}
