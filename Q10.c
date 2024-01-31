fc28747515669785a4a8c40a82bf1da9d0620069
hyemin <88538911+hhyem@users.noreply.github.com>
2024-01-31 12:04:03 +0900

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
+int main(void) {
+  int a, b;
+  int sum = 0;
+  
+  printf("a값 : ");
+  scanf("%d", &a);
+  printf("b값 : ");
+  scanf("%d", &b);
 
-int main() {
-    int a, b;
-    printf("a의 값 : ");
-    scanf("%d", &a);
-    printf("b의 값 : ");
+  while(a >= b) {
+    printf("a보다 큰 값을 입력하세요!\n");
+    printf("b값 : ");
     scanf("%d", &b);
+  }
 
-    do {
-        printf("a보다 큰 값을 입력하세요!\n");
-        printf("b의 값 : ");
-        scanf("%d", &b);
-    } while (a > b);
+  printf("b - a는 %d입니다.\n", b - a);
 
-    printf("b - a는 %d입니다. \n", b-a);
-
-    return 0;
+  return 0;
 }
