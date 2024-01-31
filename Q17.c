cc1c353822791dad1cddafce7d2ce2b71381a135
hyemin <88538911+hhyem@users.noreply.github.com>
2024-01-31 12:54:36 +0900

    Update Q17.c

--- Q17.c
+++ Q17.c
@@ -45,3 +45,32 @@ int main(void) {
   spira(n);
   
 }
+
+//내가 짠 코드(어째 더 퇴화하는 듯한..모습이)
+#include <stdio.h>
+
+void spira(int n) {
+  int s = (n-1) * 2 + 1; //최종 열 개수 (마지막 열)
+  int t = (s-1) / 2; //공백 
+  int star = 0; //* 출력할 개수 
+  for(int i=1; i<=n; i++) {
+    for(int j=0; j<1; j++) {
+      for(int k=0; k<t; k++)
+        printf(" ");
+      star = ((i-1) * 2 + 1);
+      for(int z=0; z<star; z++)
+        printf("*");
+      t-=1;
+    }
+    printf("\n");
+  }
+}
+
+int main(void) {
+  int n;
+  printf("높이 : ");
+  scanf("%d", &n);
+  spira(n);
+  
+  return 0;
+}
