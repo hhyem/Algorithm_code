6748e3fc1cdb401c847b80f9bd947114ec6c1bce
hyemin <88538911+hhyem@users.noreply.github.com>
2024-01-31 12:17:41 +0900

    Update Q12.c

--- Q12.c
+++ Q12.c
@@ -19,3 +19,26 @@ int main() {
 
     return 0;
 }
+
+
+//2번째 버전
+#include <stdio.h>
+
+int main(void) {
+
+  printf("  |  1");
+  for(int i=2; i<=9; i++) {
+    printf("%3d", i);
+  }
+  printf("\n--+-------------------------------\n");
+  
+  for(int i=1; i<=9; i++) {
+    printf("%2d|", i);
+    for(int j=1; j<=9; j++) {
+      printf(" %2d", i * j);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
